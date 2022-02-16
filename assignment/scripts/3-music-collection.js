058aac9b7e4232a2f6e9f84f096132754fe7d2aa
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-16 10:55:56 -0600

    3Music-collection,js; created collection var, myMusic Var to accept input parameters

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,11 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+let myMusic = {
+  title: title,
+  artist: artist,
+  yearPublished: yearPublished
+
+}
+collection.push(album);
