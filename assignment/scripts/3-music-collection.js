e11aeaf245351015983a76c141aa9b3979ce798f
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-16 11:16:43 -0600

    3Music-collection,js; add function to add to collection, log'd test album

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,15 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+let collection = function addToCollection( title, artist, yearPublished ) {
 
 let myMusic = {
   title: title,
   artist: artist,
   yearPublished: yearPublished
-
 }
-collection.push(album);
+
+collection.push(album);//commit1
+return myMusic;
+};
+
+console.log('Rappers Delight', 'Sugarhill Gang', 1979);
