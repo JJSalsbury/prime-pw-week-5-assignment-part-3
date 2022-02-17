de32845e57185043e8d3eb9f019d1932e65e6dcb
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-17 13:38:43 -0600

    3Music-collection,js; added title/artist/yrpublic to take album name/value pairs, created showCollection function; console checked

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,20 +1,34 @@
 console.log('***** Music Collection *****')
 
-let collection = function addToCollection( title, artist, yearPublished ) {
+let collection = [];
 
-let myMusic = {
+function addToCollection( title, artist, yearPublished ) {
+ title = title;
+ artist = artist;
+ yearPublished = yearPublished;
+
+let album = {
   title: title,
   artist: artist,
   yearPublished: yearPublished
 }
 
 collection.push(album);
-  return myMusic;
-};
+  return album;
+}
 
-console.log('Rappers Delight', 'Sugarhill Gang', 1979);
-console.log('Hurt', 'Nine Inch Nails', 1995);
-console.log('Come and Get Your Love', 'Redbone', 1973);
-console.log('Bring It On Home To Me', 'Sam Cooke', 1962);
-console.log('All I Want', 'Kodaline', 2012);
-console.log('Nantes', 'Beirut', 2007);
+console.log( addToCollection('Sittin On The Dock of the Bay', 'Otis Redding', 1968));
+console.log( addToCollection('Hurt', 'Nine Inch Nails', 1995));
+console.log( addToCollection('Come and Get Your Love', 'Redbone', 1973));
+console.log( addToCollection('Bring It On Home To Me', 'Sam Cooke', 1962));
+console.log( addToCollection('Try a Little Tenderness', 'Otis Redding', 1966));
+console.log( addToCollection('Hush', 'Deep Purple', 1968));//console log 2
+console.log( collection );
+
+function showCollection ( array ) {
+  console.log( 'Albums in collection:', array.length );
+  for (let i = 0; i < array.length; i++) {
+    console.log( array[i].title, "by", array[i].artist, "published in", array[i].yearPublished );
+  }
+}
+showCollection( collection );
