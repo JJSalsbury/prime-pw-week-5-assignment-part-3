290de11ee48e67d7a89ec4949c5fcd00d9ca2b33
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-20 14:09:48 -0600

    3Music-collection,js: final review and complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -17,20 +17,22 @@ collection.push(album);
   return album;
 }
 
-console.log( addToCollection('Sittin On The Dock of the Bay', 'Otis Redding', 1968));
-console.log( addToCollection('Hurt', 'Nine Inch Nails', 1995));
-console.log( addToCollection('Come and Get Your Love', 'Redbone', 1973));
-console.log( addToCollection('Bring It On Home To Me', 'Sam Cooke', 1962));
-console.log( addToCollection('Try a Little Tenderness', 'Otis Redding', 1966));
-console.log( addToCollection('Hush', 'Deep Purple', 1968));//console log 2
-console.log( collection );
+console.log( 'Adding 6 albums to my collection:' );
+console.log( 'ADD TO COLLECTION:', addToCollection('Sittin On The Dock of the Bay', 'Otis Redding', 1968));
+console.log( 'ADD TO COLLECTION:', addToCollection('Hurt', 'Nine Inch Nails', 1995));
+console.log( 'ADD TO COLLECTION:', addToCollection('Come and Get Your Love', 'Redbone', 1973));
+console.log( 'ADD TO COLLECTION:', addToCollection('Bring It On Home To Me', 'Sam Cooke', 1962));
+console.log( 'ADD TO COLLECTION:', addToCollection('Try a Little Tenderness', 'Otis Redding', 1966));
+console.log( 'ADD TO COLLECTION:', addToCollection('Hush', 'Deep Purple', 1968));
+console.log( 'COLLECTION ARRAY: Test- (number of albums in collection):', collection );
 
 function showCollection ( array ) {
-  console.log( 'Albums in collection:', array.length );
+  console.log( 'SHOW COLLECTION:  Test- (albums in collection):', array.length );
   for (let i = 0; i < array.length; i++) {
     console.log( array[i].title, "by", array[i].artist, "published in", array[i].yearPublished );
   }
 }
+
 showCollection( collection );
 
 function findByArtist ( artist ) {
@@ -42,5 +44,32 @@ function findByArtist ( artist ) {
   }
   return artistName;
 }
-console.log(findByArtist('Otis Redding'));
-console.log(findByArtist('Ray Charles'));
+console.log('FIND BY ARTIST: Test- (search Otis Redding, should log 2 results):', findByArtist('Otis Redding'));
+console.log('FIND BY ARTIST: Test- (search Ray Charles, should log empty array):', findByArtist('Ray Charles'));
+console.log('FIND BY ARTIST: Test- (search Redbone, should log 1 result):', findByArtist('Redbone'));
+
+function search ( searchObject ) {
+  let searchCollection = [];
+
+  if ( ('artist' in searchObject) && ('year' in searchObject) ) {
+    for (let i = 0; i < collection.length; i++) {
+      if (searchObject.artist === collection[i].artist && searchObject.year === collection[i].yearPublished) {
+        searchCollection.push(searchObject);
+        }
+      }
+      return searchCollection;
+    }
+    return collection;
+  }
+
+test1 = {
+  artist: 'Ray Charles',
+  year: 1957 }
+
+console.log('SEARCH: Test- (search Ray Charles by artist and year search criteria, should log empty array):', search(test1));
+
+test2 = {
+  artist: 'Otis Redding',
+  year: 1966 }
+
+console.log('SEARCH: Test- (search Otis Redding by artist and year search criteria, should log 1 results):', search(test2));
